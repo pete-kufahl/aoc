d051f38fd9184ec63b715427362319dd3d780932
Peter Kufahl <pkufahl@gmail.com>
2024-11-30 22:21:33 -0700

    day6 puzzle 1 python

---
+++ 2015/python/day6/lights.py
@@ -0,0 +1,57 @@
+"""
+bool_matrix = np.full((2, 2), True)
+print(bool_matrix)
+"""
+import numpy as np
+
+if __name__ == "__main__":
+
+    file_name = "input.txt"
+    grid = np.full((1000, 1000), False)
+    instrs = 0
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+
+                parts = line.strip().split(' ')
+                # toggle 756,965 through 812,992
+                # turn on 489,959 through 759,964
+                if len(parts) in [4, 5]:
+                    instrs += 1
+                    if parts[0] == 'turn':
+                        start = parts[2].split(',')
+                        x0 = int(start[0])
+                        y0 = int(start[1])
+                        end = parts[4].split(',')
+                        x1 = int(end[0])
+                        y1 = int(end[1])
+                        if parts[1] == 'on':
+                            for x in range(x0, x1+1):
+                                for y in range(y0, y1 + 1):
+                                    grid[x, y] = True
+                        elif parts[1] == 'off':
+                            for x in range(x0, x1+1):
+                                for y in range(y0, y1 + 1):
+                                    grid[x, y] = False
+                        else:
+                            raise RuntimeError('unknown turn on/off/? line', line)
+                    elif parts[0] == 'toggle':
+                        start = parts[1].split(',')
+                        x0 = int(start[0])
+                        y0 = int(start[1])
+                        end = parts[3].split(',')
+                        x1 = int(end[0])
+                        y1 = int(end[1])
+                        for x in range(x0, x1+1):
+                                for y in range(y0, y1 + 1):
+                                    grid[x, y] = not grid[x, y]
+                    else:
+                        raise RuntimeError('unknown line', line)
+                    
+        count = np.count_nonzero(grid)
+        print(f'after {instrs} instructions, {count} lights on')
+
+    except FileNotFoundError:
+        print(f"The file {file_name} was not found.")
+    except IOError:
+        print("An error occurred while reading the file.")
