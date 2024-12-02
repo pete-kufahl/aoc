3ab84f61784487f86aa04f07bb2120cfa3421b91
Peter Kufahl <pkufahl@gmail.com>
2024-12-01 21:30:16 -0700

    cleanup

--- 2024/java/day1/CompareLists.java
+++ 2024/java/day1/CompareLists.java
@@ -2,14 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.String;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CompareLists {
 
