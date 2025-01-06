9fa10fd9cccc6ea395cc06dc0b792ee8b5937537
Peter Kufahl <pkufahl@gmail.com>
2025-01-05 17:10:48 -0700

    solved day 9 part 1 python

--- 2024/python/day9/compact.py
+++ 2024/python/day9/compact.py
@@ -20,49 +20,91 @@ def PART_ONE(filepath, debug=False):
             # odds -> memory space of size char
             if input_idx % 2:
                 space = int(char)
-                debug and print(f'at map index {map_idx}, free space of {space}')
+                # debug and print(f'at map index {map_idx}, free space of {space}')
                 gapMap[map_idx] = space
                 map_idx += space
             else:
                 filesize = int(char)
-                debug and print(f'at map index {map_idx}, file id {fileID} of size {filesize}')
+                # debug and print(f'at map index {map_idx}, file id {fileID} of size {filesize}')
                 diskMap[map_idx] = (fileID, filesize)
                 fileID += 1
                 map_idx += filesize
 
+    debug and print (f'map index at start of compression = {map_idx}')
     if debug:
-        i = 0
-        disk_str = ""
-        while i < map_idx:
-            if i in diskMap:
-                fid, fsize = diskMap[i]
-                for _ in range(fsize):
-                    disk_str += str(fid)
-                    i += 1
-            else:
-                disk_str += '.'
-                i += 1
-        print (disk_str)
-        for k, v in diskMap.items():
+        print_disc(diskMap, map_idx)
+        """ for k, v in diskMap.items():
             print (f'file id {v[0]}, map index {k}, size {v[1]}')
         for l, w in gapMap.items():
-            print (f'map index {l}, gap of size {w}')
+            print (f'map index {l}, gap of size {w}') """
 
     uncompressed = sorted(diskMap.keys())
-    print(uncompressed)
-    print(uncompressed[-1])
+    gaps = sorted(gapMap.keys())
     
     checksum: int = 0    # python just handles big integers
-    while len(gapMap) > 0 and len(diskMap) > 0:
-        gap_ptr = 0
-        file_idx = uncompressed[-1]
-        fid, fsize = diskMap[file_idx]
+    cidx = 0
 
+    end = map_idx
+    while cidx < end:
+        if cidx in diskMap:
+            fid, fsize = diskMap[cidx]
+            for _ in range(fsize):
+                debug and print(f'file content at {cidx}, fid = {fid}, size = {fsize}, end index: {end}')
+                checksum += (cidx * fid)
+                cidx += 1
+        elif cidx in gapMap:
+            lastfile = uncompressed[-1]
+            firstgap = cidx
+            fid, fsize = diskMap[lastfile]
+            gapsize = gapMap[firstgap]
+            while (gapsize > 0) and (fsize > 0):
+                debug and print(f'gap at {cidx} (size={gapsize}), moving file {fid} from {lastfile} (size= {fsize}), end index: {end} -> {end-1}')
+                gapsize -= 1
+                fsize -= 1
+                checksum += (cidx * fid)
+                end -= 1
+                cidx += 1
+            if gapsize > 0:
+                gapMap.pop(firstgap)    # update gapmap - remove old gap
+                gapMap[cidx] = gapsize  # update gapmap - replace with smaller gap
+                debug and print(f'new GAP of {gapsize} starting at {cidx}')
+                diskMap.pop(uncompressed.pop(-1))    # remove file
+            elif fsize > 0:
+                gapMap.pop(firstgap)                # remove gap
+                diskMap[lastfile] = (fid, fsize)    # update filemap with smaller file
+                debug and print(f'leftover FILE id={fid}, size={fsize}')
+            else:
+                debug and print(f'file perfectly filled gap at {cidx-1}')
+                gapMap.pop(firstgap)    # remove gap
+                diskMap.pop(uncompressed.pop(-1))    # remove file
+        else:
+            print(f'index {cidx} not registered in diskmap or gapmap, i don\'t know what to do!')
+            cidx += 1
 
-        
+        debug and print_disc(diskMap, end)
+        if uncompressed:
+            lid, lsize = diskMap[uncompressed[-1]]
+            while (end - 1) > (uncompressed[-1] + lsize - 1):
+                debug and print('leftover gap at end! adjusting ...')
+                end -= 1
+                debug and print_disc(diskMap, end)
 
+    print (f'final checksum is {checksum}') 
 
-
+def print_disc(diskMap, map_idx):
+    i = 0
+    disk_str = ""
+    while i < map_idx:
+        if i in diskMap:
+            fid, fsize = diskMap[i]
+            for _ in range(fsize):
+                disk_str += str(fid)
+                i += 1
+        else:
+            disk_str += '.'
+            i += 1
+    print (disk_str, map_idx)           
+   
 
 if __name__ == '__main__':
 
@@ -70,5 +112,5 @@ if __name__ == '__main__':
     parser.add_argument('file_path', help="Path to the text file containing the one-line string.")
     args = parser.parse_args()
     
-    PART_ONE(args.file_path, True)
+    PART_ONE(args.file_path, False)
     # PART_TWO(args.file_path, True)
