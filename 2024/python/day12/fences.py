e0400601eca210f50948315de42ba1b09d341ce6
Peter Kufahl <pkufahl@gmail.com>
2025-01-15 23:18:20 -0700

    BFS

--- 2024/python/day12/fences.py
+++ 2024/python/day12/fences.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import deque
 
 def read_grid(file_path):
     """
@@ -10,18 +11,61 @@ def read_grid(file_path):
 
     return grid
 
-def in_bounds(node, grid):
+def in_bounds(r, c, grid):
+    return (-1 < r < len(grid)) and (-1 < c < len(grid[0]))
+
+def area(region):
+    return len(region)
+
+def perimeter(region):
     """
-    check x against columns, y against rows
+    find the perimeter of a connected region, made of a set of tuples
     """
-    return (-1 < node[0] < len(grid[0])) and (-1 < node[1] < len(grid))
+    pass
+
 
 def PART_ONE(grid, debug=False):
     """
     calculate fence boundaries in grid of text
     each region uses a fence that costs its area * its perimeter
     """
-    print(f'input grid is {len(grid)} rows and {len(grid[0])} columns')
+    rows = len(grid)
+    cols = len(grid[0])
+    print(f'input grid is {rows} rows and {cols} columns')
+    
+    regions = []
+    visited = set()
+
+    # visit all locations in farm just once
+    for r in range(rows):
+        for c in range(cols):
+            if (r, c) in visited: continue
+            visited.add((r, c))
+            curr_region = set()
+            curr_plant = grid[r][c]
+            # now perform BFS to find all locations in current region
+            # 1. initial queue has the starting point
+            qBFS = deque[(r, c)]
+            # 2. visit all neighbors
+            while(qBFS):
+                curr_r, curr_c = qBFS.popleft()
+                # 2a. iterate through the directions to find neighboring (unseen) matching locations
+                for next_r, next_c in [(curr_r-1, curr_c), (curr_r+1, curr_c), (curr_r, curr_c-1), (curr_r, curr_c+1)]:
+                    if not in_bounds(next_r, next_c, grid): continue
+                    if grid[next_r][next_c] != curr_plant: continue
+                    if (next_r, next_c) in curr_region: continue
+                    # 2b. add qualifying locations to the region and queue
+                    curr_region.add((next_r, next_c))
+                    qBFS.append((next_r, next_c))
+            # 3. add region to the set of visited locations
+            visited = visited | curr_region
+            # 4. add region to the collection of regions
+            regions.append(curr_region)
+
+
+
+
+
 
 if __name__ == '__main__':
 
