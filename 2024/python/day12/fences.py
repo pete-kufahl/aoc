6c812c7f3c029a6f66721ae57a509b2646f00da1
Peter Kufahl <pkufahl@gmail.com>
2025-01-15 23:48:57 -0700

    day 12 part 1 python

--- 2024/python/day12/fences.py
+++ 2024/python/day12/fences.py
@@ -20,8 +20,17 @@ def area(region):
 def perimeter(region):
     """
     find the perimeter of a connected region, made of a set of tuples
+    each location in the set has a perimeter of 4,
+    but for every shared edge with another location, substract that locations perimeter by 1
     """
-    pass
+    total_perimeter = 0
+    for curr_r, curr_c in region:
+        total_perimeter += 4
+        for next_r, next_c in [(curr_r-1, curr_c), (curr_r+1, curr_c), (curr_r, curr_c-1), (curr_r, curr_c+1)]:
+            if (next_r, next_c) in region:
+                total_perimeter -= 1
+
+    return total_perimeter
 
 
 def PART_ONE(grid, debug=False):
@@ -31,7 +40,7 @@ def PART_ONE(grid, debug=False):
     """
     rows = len(grid)
     cols = len(grid[0])
-    print(f'input grid is {rows} rows and {cols} columns')
+    debug and print(f'input grid is {rows} rows and {cols} columns')
     
     regions = []
     visited = set()
@@ -39,19 +48,22 @@ def PART_ONE(grid, debug=False):
     # visit all locations in farm just once
     for r in range(rows):
         for c in range(cols):
+            # start_pt = (r, c)
             if (r, c) in visited: continue
             visited.add((r, c))
-            curr_region = set()
+            curr_region = {(r, c)}
             curr_plant = grid[r][c]
+
             # now perform BFS to find all locations in current region
             # 1. initial queue has the starting point
-            qBFS = deque[(r, c)]
+            qBFS = deque([(r, c)])
             # 2. visit all neighbors
-            while(qBFS):
+            while qBFS:
                 curr_r, curr_c = qBFS.popleft()
                 # 2a. iterate through the directions to find neighboring (unseen) matching locations
                 for next_r, next_c in [(curr_r-1, curr_c), (curr_r+1, curr_c), (curr_r, curr_c-1), (curr_r, curr_c+1)]:
-                    if not in_bounds(next_r, next_c, grid): continue
+                    # if not in_bounds(next_r, next_c, grid): continue
+                    if next_r < 0 or next_c < 0 or next_r >= rows or next_c >= cols: continue
                     if grid[next_r][next_c] != curr_plant: continue
                     if (next_r, next_c) in curr_region: continue
                     # 2b. add qualifying locations to the region and queue
@@ -61,10 +73,14 @@ def PART_ONE(grid, debug=False):
             visited = visited | curr_region
             # 4. add region to the collection of regions
             regions.append(curr_region)
+    
 
+    if debug:
+        for region in regions:
+            print(f'area: {area(region)}, perimeter: {perimeter(region)}')
 
-
-
+    fences = sum(area(region) * perimeter(region) for region in regions)
+    print(f'total fence units is {fences} for {len(regions)} regions')
 
 
 if __name__ == '__main__':
