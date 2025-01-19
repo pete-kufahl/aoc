20706c00be33acc29ab25fe0b95325bbca275045
Peter Kufahl <pkufahl@gmail.com>
2025-01-19 15:37:19 -0700

    day 15 part 2 python

--- 2024/python/day15/warehouse.py
+++ 2024/python/day15/warehouse.py
@@ -59,7 +59,7 @@ def track_robot_small_boxes(grid, robot, debug):
             print(*row, sep="")
     return grid
 
-def score_grid_small_boxes(grid):
+def score_grid(grid, box='O'):
     """
     use generator expression
     sum the score for each box at (row, col): 100 * row + 1 * col
@@ -69,7 +69,7 @@ def score_grid_small_boxes(grid):
     return sum(100 * r + c 
                for r in range(rows) 
                for c in range(cols)
-               if grid[r][c] == 'O')
+               if grid[r][c] == box)
 
 
 def expand_grid(grid):
@@ -88,6 +88,7 @@ def expand_grid(grid):
         result.append(list(wide_str))
     return result
 
+
 def track_robot_wide_boxes(grid, robot, debug):
     """
     alter the algo for tracking the robot in the small warehouse, because we have to
@@ -104,7 +105,46 @@ def track_robot_wide_boxes(grid, robot, debug):
         delr = { '^': -1, 'v': 1 }.get(move, 0)
         delc = { '<': -1, '>': 1 }.get(move, 0)
 
-    
+        targets = [(r,c)]       # intended move targets, starting with the robot position
+        can_move = True         # whether the move happens 
+
+        # find all the targets for this move
+        # iterate through targets, adding to it when [] enountered.
+        #.  note: not the safest thing to do, can only do with list
+        for tr, tc in targets:
+            nr = tr + delr
+            nc = tc + delc
+            if (nr, nc) in targets: continue    # avoid double-add
+            loc = grid[nr][nc]
+            if loc == '#':      # there's a wall in the way
+                can_move = False
+                break
+            if loc == '[':      # left half of box
+                targets.append((nr, nc))
+                targets.append((nr, nc + 1))
+            if loc == ']':      # right half of box
+                targets.append((nr, nc))
+                targets.append((nr, nc - 1))
+            # no need for '.' check, as all targets must end before a wall
+            #. for can_move to remain True
+        if can_move:
+            # make a deep copy of the grid, to modify grid with previous version on the right side
+            prevgrid = [list(row) for row in grid]
+            # robot, and any boxes in front of it, move by one space
+            debug and print(f'moving ({move}) robot to ({r + delr}, {c + delc}) with {len(targets) - 1} boxes')
+            grid[r][c] = '.'                # robot vacates position
+            grid[r + delr][c + delc] = '@'  # robot's new location
+            for box_r, box_c in targets[1:]:    # 1. target positions set to .
+                grid[box_r][box_c] = '.'
+            for box_r, box_c in targets[1:]:    # 2. override those . with moved-to targets
+                grid[box_r + delr][box_c + delc] = prevgrid[box_r][box_c]
+            r += delr
+            c += delc         
+    if debug:
+        for row in grid:
+            print(*row, sep="")
+    return grid    
+
 
 def PART_ONE(grid, robot, debug):
     if debug:
@@ -112,16 +152,22 @@ def PART_ONE(grid, robot, debug):
             print(line)
         print(robot)
     result_grid = track_robot_small_boxes(grid, robot, debug)
-    print(f'final GPS score (small boxes) = {score_grid_small_boxes(result_grid)}')
+    score = score_grid(result_grid)
+    print(f'final GPS score (small boxes) = {score}')
+
 
 def PART_TWO(grid, robot, debug):
+    """
+    when computing the score, the [ side of the box is closest to the left edge
+    """
     wide_grid = expand_grid(grid)
     if debug:
         for line in wide_grid:
             print(line)
         print(robot)
     result_grid = track_robot_wide_boxes(wide_grid, robot, debug)
-    # print(f'final GPS score = {score_grid_wide_boxes(result_grid)}')
+    score = score_grid(result_grid, '[')
+    print(f'final GPS score (wide boxes) = {score}')
 
 
 if __name__ == '__main__':
@@ -132,4 +178,7 @@ if __name__ == '__main__':
 
     PART_ONE(grid, robot, debug=False)
     print()
+
+    # reset grid
+    grid, robot = parse_file(args.file_path)
     PART_TWO(grid, robot, debug=True)
